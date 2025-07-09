400eda901e06f187f6a368e39d0a0274e26e94d3
Mohammad Al Ghabra <114345689+dustycd@users.noreply.github.com>
2025-07-09 19:15:17 +0300

    Updated Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -87,13 +87,6 @@ const Hero = () => {
           </div>
         </div>
         
-        {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
-          <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center">
-            <div className="w-1 h-3 bg-white/50 rounded-full mt-2 animate-pulse"></div>
-          </div>
-        </div>
-        
         {/* WhatsApp Button */}
         <WhatsAppButton 
           message="Hello! I'm interested in starting a business in UAE. Can you provide me with more information about your services?"
